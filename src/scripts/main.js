6395588ebb56d0b99b612952c1c4a5b190da16fb
Hanna <anutalahmaniuk@mail.com>
2025-08-21 15:37:08 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+
+let cleanSpans = [...spans];
+
+cleanSpans = cleanSpans.map((span) => {
+  return Number(span.innerText.replaceAll(',', ''));
+});
+
+const total = cleanSpans.reduce((result, num) => {
+  return result + num;
+}, 0);
+
+const average = Math.round(total / cleanSpans.length);
+
+document.querySelector('.average-population').innerText = String(average);
+
+document.querySelector('.total-population').innerText = total.toLocaleString();
